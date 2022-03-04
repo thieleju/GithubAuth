e7159c2761d44b9ea8446ab593deddc06fb24ca6
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-03-04 23:19:25 +0100

    Added vuetify and simple express api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
   <v-app>
+    <v-navigation-drawer color="grey-darken-2" permanent>
+      <router-link
+        v-for="item in apps"
+        :to="item.name"
+        :key="item.name"
+        class=""
+      >
+        <v-list-item :title="item.title"></v-list-item>
+      </router-link>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block> Logout </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="grey-darken-1" elevation="0" priority="-1">
+      <div class="d-flex justify-center align-center w-100"></div
+    ></v-app-bar>
+
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card height="200px" class="d-flex justify-center align-center">
+          <router-view></router-view>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+interface AppItem {
+  title: string;
+  name: string;
+  icon: string;
+}
 
-export default defineComponent({
-  name: 'App',
-
-  components: {
-    HelloWorld,
+const apps: AppItem[] = [
+  {
+    title: "Home",
+    name: "home",
+    icon: "mdi-home",
   },
-
-  data () {
-    return {
-      //
-    }
+  {
+    title: "About",
+    name: "about",
+    icon: "mdi-information",
   },
-})
+];
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,6 +5,16 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  {
+    theme: {
+      defaultTheme: "customDarkTheme",
+      themes: {
+        customDarkTheme: {
+          dark: false,
+        }
+      }
+    }
+  }
 )
